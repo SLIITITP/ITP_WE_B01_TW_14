984bd50ededc41f6150025e02710f18ff94f53be
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2023-05-12 22:06:34 +0530

    customer screen

--- client/src/screens/CustomerScreen.js
+++ client/src/screens/CustomerScreen.js
@@ -1,61 +1,160 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import AuthContext from '../context/AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
+import { Modal } from 'react-bootstrap';
 
 function CustomerScreen() {
   const navigate = useNavigate();
   const { user } = useContext(AuthContext);
+  const [customers, setCustomers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [modalData, setModalData] = useState({});
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    async function fetchData() {
+      try {
+        const res = await fetch(`http://localhost:8000/api/customers`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        });
+        const result = await res.json();
+        if (!result.error) {
+          setCustomers(result);
+          setLoading(false);
+        } else {
+          console.log(result);
+          setLoading(false);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>Customers</h1>
-      <div className="d-flex justify-content-between">
-        <a href="/customerinfo" className="btn btn-danger my-2">
-          Reload Customer List
-        </a>
-        <div>
-          <Link
-            className="btn btn-info mb-2"
-            to={'/createcustomer'}
-            role="button"
-          >
-            Add New Customer
-          </Link>
+      <div>
+        <h1>Customers</h1>
+        <div className="d-flex justify-content-between">
+          <a href="/customerinfo" className="btn btn-danger my-2">
+            Reload Customer List
+          </a>
+          <div>
+            <Link
+              className="btn btn-info mb-2"
+              to={'/createcustomer'}
+              role="button"
+            >
+              Add New Customer
+            </Link>
+          </div>
         </div>
-      </div>
-      <hr className="my-4" />
+        <hr className="my-4" />
 
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th scope="col">Customer Id</th>
-            <th scope="col">Customer Name</th>
-            <th scope="col">Company Name</th>
-            <th scope="col">Address</th>
-            <th scope="col">Email</th>
-            <th scope="col">Phone</th>
-            <th scope="col">NIC</th>
-            <th scope="col">Credit Limit</th>
-            <th scope="col">Credit Days</th>
-          </tr>
-        </thead>
-        {/* <tbody>
-                    {contacts.map((contact) => (
-                      <tr
-                        key={contact._id}
-                        onClick={() => {
-                          setModalData({});
-                          setModalData(contact);
-                          setShowModal(true);
-                        }}
-                      >
-                        <th scope="row">{contact.name}</th>
-                        <td>{contact.address}</td>
-                        <td>{contact.email}</td>
-                        <td>{contact.phone}</td>
-                      </tr>
-                    ))}
-                  </tbody> */}
-      </table>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Customer Id</th>
+              <th scope="col">Customer Name</th>
+              <th scope="col">Company Name</th>
+              <th scope="col">Address</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+              <th scope="col">NIC</th>
+              <th scope="col">Credit Limit</th>
+              <th scope="col">Credit Days</th>
+            </tr>
+          </thead>
+          <tbody>
+            {customers.map((customer) => (
+              <tr
+                key={customer._id}
+                onClick={() => {
+                  setModalData({});
+                  setModalData(customer);
+                  setShowModal(true);
+                }}
+              >
+                <th scope="row">{customer.customerId}</th>
+                <td>{customer.cusName}</td>
+                <td>{customer.companyName}</td>
+                <td>{customer.address}</td>
+                <td>{customer.email}</td>
+                <td>{customer.contactNo}</td>
+                <td>{customer.cusNIC}</td>
+                <td>{customer.creditLimit}</td>
+                <td>{customer.creditDays}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          {/* <Modal.Title>{modalData.firstname}</Modal.Title> */}
+          <Modal.Title>Southern Agro</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <h3>{modalData.customerId}</h3>
+          <p>
+            <strong>Customer Name</strong>: {modalData.cusName}
+          </p>
+          <p>
+            <strong>Company Name</strong>: {modalData.companyName}
+          </p>
+          <p>
+            <strong>Address</strong>: {modalData.address}
+          </p>
+          <p>
+            <strong>Email</strong>: {modalData.email}
+          </p>
+          <p>
+            <strong>Contact No</strong>: {modalData.contactNo}
+          </p>
+          <p>
+            <strong>Customer NIC</strong>: {modalData.cusNIC}
+          </p>
+          <p>
+            <strong>Credit Limit</strong>: {modalData.creditLimit}
+          </p>
+          <p>
+            <strong>Credit Days</strong>: {modalData.creditDays}
+          </p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Link className="btn btn-info" to={`/editemp/${modalData._id}`}>
+            Edit
+          </Link>
+          <button
+            className="btn btn-danger"
+            //onClick={() => modalData && deleteEmployee(modalData._id)}
+          >
+            Delete
+          </button>
+
+          {/* <button
+        className="btn btn-danger"
+        onClick={() => deleteContact(modalData._id)}
+      >
+        Delete
+      </button> */}
+
+          <button
+            className="btn btn-warning"
+            onClick={() => setShowModal(false)}
+          >
+            Close
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
--- server/models/customerModel.js
+++ server/models/customerModel.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const customerSchema = mongoose.Schema({
+  customerId: {
+    type: String,
+    unique: true,
+  },
   cusName: {
     type: String,
     required: true,
@@ -37,6 +41,38 @@ const customerSchema = mongoose.Schema({
     type: Number,
     required: true,
   },
+});
+
+customerSchema.pre('save', async function (next) {
+  try {
+    if (!this.customerId) {
+      let count = await this.constructor.countDocuments({});
+
+      let id = `CUS${(count + 1).toString().padStart(3, '0')}`;
+
+      let duplicate = true;
+
+      //   Check if id already exists in the database
+      while (duplicate) {
+        const existingCustomer = await this.constructor.findOne({
+          customerId: id,
+        });
+
+        if (!existingCustomer) {
+          duplicate = false;
+        } else {
+          count++;
+
+          id = `CUS${(count + 1).toString().padStart(3, '0')}`;
+        }
+      }
+
+      this.customerId = id;
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 const Customer = mongoose.model('Customer', customerSchema);
