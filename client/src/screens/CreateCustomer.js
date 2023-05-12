b8531e21228a4ffc91b036afa9830892507bb392
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2023-05-12 20:02:48 +0530

    Create customer

--- client/src/screens/CreateCustomer.js
+++ client/src/screens/CreateCustomer.js
@@ -28,7 +28,7 @@ function CreateCustomer() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch(`http://localhost:3000/api/customers`, {
+    const res = await fetch(`http://localhost:8000/api/customers`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -39,7 +39,7 @@ function CreateCustomer() {
 
     const result = await res.json();
     if (!result.error) {
-      toast.success(`Created [${userDetails.companyName}] Customer`);
+      toast.success(`Created ${userDetails.companyName} Customer`);
 
       setUserDetails({
         cusName: '',
@@ -56,179 +56,211 @@ function CreateCustomer() {
       toast.error(result.error);
     }
   };
+
+  const handleClear = () => {
+    setUserDetails({
+      cusName: '',
+      companyName: '',
+      address: '',
+      contactNo: '',
+      cusNIC: '',
+      district: '',
+      email: '',
+      creditLimit: '',
+      creditDays: '',
+    });
+  };
+
   return (
     <div>
-      <h2>Add Customer</h2>
-
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="cusNameInput" className="form-label mt-4">
-            Name Of the Customer
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="customernameInput"
-            name="cusName"
-            value={userDetails.cusName}
-            onChange={handleInputChange}
-            placeholder="John Doe"
-            required
-          />
+      <h2 className="text-center bg-darkgreen p-2">Add Customer</h2>
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-8">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="cusNameInput" className="form-label mt-4">
+                Name Of the Customer
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="customernameInput"
+                name="cusName"
+                value={userDetails.cusName}
+                onChange={handleInputChange}
+                placeholder="John Doe"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="companyNameInput" className="form-label mt-4">
+                Name Of the Company
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="companyNameInput"
+                name="companyName"
+                value={userDetails.companyName}
+                onChange={handleInputChange}
+                placeholder="Hayleys"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="addressInput" className="form-label mt-4">
+                Address
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="addressInput"
+                name="address"
+                value={userDetails.address}
+                onChange={handleInputChange}
+                placeholder="45/A, Galle"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="contactNoInput" className="form-label mt-4">
+                Contact Number
+              </label>
+              <input
+                type="tel"
+                className="form-control"
+                id="contactNoInput"
+                name="contactNo"
+                maxLength={10}
+                minLength={10}
+                value={userDetails.contactNo}
+                onChange={handleInputChange}
+                placeholder="0XXXXXXXXX"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="cusNICInput" className="form-label mt-4">
+                Customer NIC
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="cusNICInput"
+                name="cusNIC"
+                maxLength={12}
+                minLength={10}
+                value={userDetails.cusNIC}
+                onChange={handleInputChange}
+                placeholder="000000000000"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="districtInput" className="form-label mt-4">
+                District
+              </label>
+              <br />
+              <select
+                id="districtInput"
+                name="district"
+                value={userDetails.district}
+                onChange={handleInputChange}
+                required
+              >
+                <option value="Ampara">Ampara</option>
+                <option value="Anuradhapura">Anuradhapura</option>
+                <option value="Badulla">Badulla</option>
+                <option value="Batticaloa">Batticaloa</option>
+                <option value="Colombo">Colombo</option>
+                <option value="Galle">Galle</option>
+                <option value="Gampaha">Gampaha</option>
+                <option value="Hambantota">Hambantota</option>
+                <option value="Jaffna">Jaffna</option>
+                <option value="Kalutara">Kalutara</option>
+                <option value="Kandy">Kandy</option>
+                <option value="Kegalle">Kegalle</option>
+                <option value="Kilinochchi">Kilinochchi</option>
+                <option value="Kurunegala">Kurunegala</option>
+                <option value="Mannar">Mannar</option>
+                <option value="Matale">Matale</option>
+                <option value="Matara">Matara</option>
+                <option value="Moneragala">Moneragala</option>
+                <option value="Mullaitivu">Mullaitivu</option>
+                <option value="Nuwara Eliya">Nuwara Eliya</option>
+                <option value="Polonnaruwa">Polonnaruwa</option>
+                <option value="Puttalam">Puttalam</option>
+                <option value="Ratnapura">Ratnapura</option>
+                <option value="Trincomalee">Trincomalee</option>
+                <option value="Vavuniya">Vavuniya</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="emailInput" className="form-label mt-4">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="emailInput"
+                name="email"
+                value={userDetails.email}
+                onChange={handleInputChange}
+                placeholder="abc@example.com"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="creditLimitInput" className="form-label mt-4">
+                Credit Limit
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="creditLimitInput"
+                name="creditLimit"
+                value={userDetails.creditLimit}
+                onChange={handleInputChange}
+                placeholder="100000.00"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="districtInput" className="form-label mt-4">
+                Credit Days
+              </label>
+              <select
+                id="creditDaysInput"
+                name="creditDays"
+                value={userDetails.creditDays}
+                onChange={handleInputChange}
+                required
+              >
+                <option value="15">15</option>
+                <option value="30">30</option>
+                <option value="45">45</option>
+                <option value="60">60</option>
+                <option value="90">90</option>
+              </select>
+              <br />
+              <br />
+            </div>
+            <div className="text-center">
+              <input
+                type="submit"
+                value="Add Customer"
+                className="btn btn-info"
+              />
+              <button
+                type="button"
+                onClick={handleClear}
+                className="btn btn-danger my-2 ml-2"
+              >
+                Clear
+              </button>
+            </div>
+          </form>
         </div>
-        <div className="form-group">
-          <label htmlFor="companyNameInput" className="form-label mt-4">
-            Name Of the Company
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="companyNameInput"
-            name="companyName"
-            value={userDetails.companyName}
-            onChange={handleInputChange}
-            placeholder="Hayleys"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="addressInput" className="form-label mt-4">
-            Address
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="addressInput"
-            name="address"
-            value={userDetails.address}
-            onChange={handleInputChange}
-            placeholder="45/A, Galle"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="contactNoInput" className="form-label mt-4">
-            Contact Number
-          </label>
-          <input
-            type="tel"
-            className="form-control"
-            id="contactNoInput"
-            name="contactNo"
-            maxLength={10}
-            minLength={10}
-            value={userDetails.contactNo}
-            onChange={handleInputChange}
-            placeholder="0XXXXXXXXX"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="cusNICInput" className="form-label mt-4">
-            Customer NIC
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="cusNICInput"
-            name="cusNIC"
-            maxLength={12}
-            minLength={10}
-            value={userDetails.cusNIC}
-            onChange={handleInputChange}
-            placeholder="000000000000"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="districtInput" className="form-label mt-4">
-            District
-          </label>
-          <br />
-          <select
-            id="districtInput"
-            name="district"
-            value={userDetails.district}
-            onChange={handleInputChange}
-            required
-          >
-            <option value="Ampara">Ampara</option>
-            <option value="Anuradhapura">Anuradhapura</option>
-            <option value="Badulla">Badulla</option>
-            <option value="Batticaloa">Batticaloa</option>
-            <option value="Colombo">Colombo</option>
-            <option value="Galle">Galle</option>
-            <option value="Gampaha">Gampaha</option>
-            <option value="Hambantota">Hambantota</option>
-            <option value="Jaffna">Jaffna</option>
-            <option value="Kalutara">Kalutara</option>
-            <option value="Kandy">Kandy</option>
-            <option value="Kegalle">Kegalle</option>
-            <option value="Kilinochchi">Kilinochchi</option>
-            <option value="Kurunegala">Kurunegala</option>
-            <option value="Mannar">Mannar</option>
-            <option value="Matale">Matale</option>
-            <option value="Matara">Matara</option>
-            <option value="Moneragala">Moneragala</option>
-            <option value="Mullaitivu">Mullaitivu</option>
-            <option value="Nuwara Eliya">Nuwara Eliya</option>
-            <option value="Polonnaruwa">Polonnaruwa</option>
-            <option value="Puttalam">Puttalam</option>
-            <option value="Ratnapura">Ratnapura</option>
-            <option value="Trincomalee">Trincomalee</option>
-            <option value="Vavuniya">Vavuniya</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="emailInput" className="form-label mt-4">
-            Email
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="emailInput"
-            name="email"
-            value={userDetails.email}
-            onChange={handleInputChange}
-            placeholder="abc@example.com"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="creditLimitInput" className="form-label mt-4">
-            Credit Limit
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="creditLimitInput"
-            name="creditLimit"
-            value={userDetails.creditLimit}
-            onChange={handleInputChange}
-            placeholder="100000.00"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="districtInput" className="form-label mt-4">
-            Credit Days
-          </label>
-          <br />
-          <select
-            id="creditDaysInput"
-            name="creditDays"
-            value={userDetails.creditDays}
-            onChange={handleInputChange}
-            required
-          >
-            <option value="15">15</option>
-            <option value="30">30</option>
-            <option value="45">45</option>
-            <option value="60">60</option>
-            <option value="90">90</option>
-          </select>
-        </div>
-        <input type="submit" value="Add Customer" className="btn btn-info" />
-      </form>
+      </div>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,31 +1,31 @@
-require("dotenv").config({ path: "./config/config.env" });
+require('dotenv').config({ path: './config/config.env' });
 
-const express = require("express");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser"); //pasindu
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser'); //pasindu
 
 //Initialize express app
 const app = express();
 
-const connectDB = require("./config/db");
+const connectDB = require('./config/db');
 
-const auth = require("./middlewares/auth");
+const auth = require('./middlewares/auth');
 
 //middleware
 app.use(express.json()); //sending repsonses in json format, this middleware will parse the data and send it in json format.
 
-app.use(morgan("tiny")); //Morgan logs useful information about HTTP requests and responses, such as the request method, the URL, the status code, and the length of the response body
+app.use(morgan('tiny')); //Morgan logs useful information about HTTP requests and responses, such as the request method, the URL, the status code, and the length of the response body
 
-app.use(require("cors")()); //used in Authorization
+app.use(require('cors')()); //used in Authorization
 
 //routes
-app.use("/api", require("./routes/auth"));
-app.use("/api/", require("./routes/contact"));
-app.use("/api/", require("./routes/employee"));
-app.use("/api/", require("./routes/salary"));
-app.use("/api/", require("./routes/attendance"));
-app.use("/api/", require("./routes/report"));
+app.use('/api', require('./routes/auth'));
+app.use('/api/', require('./routes/contact'));
+app.use('/api/', require('./routes/employee'));
+app.use('/api/', require('./routes/salary'));
+app.use('/api/', require('./routes/attendance'));
+app.use('/api/', require('./routes/report'));
 
 //if our token was valid then we will have the user in the request object.
 // app.get("/protected", auth, (req, res) => {s
@@ -34,14 +34,14 @@ app.use("/api/", require("./routes/report"));
 
 //Bhanuka***************************************************************************
 
-app.use("/api/", require("./routes/vehicle"));
-app.use("/api/", require("./routes/fuel"));
-app.use("/api/", require("./routes/runningrecord"));
-app.use("/api/", require("./routes/garage"));
-app.use("/api/", require("./routes/vehicledocument"));
-app.use("/api/", require("./routes/driver-vehicle-assign"));
-app.use("/api/", require("./routes/repairassign"));
-app.use("/api/", require("./routes/repair"));
+app.use('/api/', require('./routes/vehicle'));
+app.use('/api/', require('./routes/fuel'));
+app.use('/api/', require('./routes/runningrecord'));
+app.use('/api/', require('./routes/garage'));
+app.use('/api/', require('./routes/vehicledocument'));
+app.use('/api/', require('./routes/driver-vehicle-assign'));
+app.use('/api/', require('./routes/repairassign'));
+app.use('/api/', require('./routes/repair'));
 
 // const vehiclerouter = require("./routes/vehicle");
 // const fuelrouter = require("./routes/fuel");
@@ -66,20 +66,22 @@ app.use("/api/", require("./routes/repair"));
 //Bhanuka***************************************************************************
 
 // Ashen***************
-const categoryRoutes = require("./routes/categoryRoutes");
-const productRoutes = require("./routes/productRoutes");
-const orderRoutes = require("./routes/orderRoutes");
+const categoryRoutes = require('./routes/categoryRoutes');
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const customerRoutes = require('./routes/customerRoutes');
 
-app.use("/api/categories", categoryRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/orders", orderRoutes);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/customers', customerRoutes);
 
 // app.use("/api/", require("./routes/categoryRoutes"));
 // app.use("/api/", require("./routes/productRoutes"));
 // app.use("/api/", require("./routes/orderRoutes"));
 //new
-app.use("/api/", require("./routes/auth"));
-app.get("/protected", auth, (req, res) => {
+app.use('/api/', require('./routes/auth'));
+app.get('/protected', auth, (req, res) => {
   return res.status(200).json({ ...req.user._doc });
 });
 
@@ -116,13 +118,13 @@ app.use((err, req, res, next) => {
 // Hasa*************************************************
 
 //Pasindu***************************************************************************
-const cors = require("cors");
+const cors = require('cors');
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/api", require("./routes/deliveries"));
-app.use("/api", require("./routes/schedules"));
-app.use("/api", require("./routes/deliveryreport"));
+app.use('/api', require('./routes/deliveries'));
+app.use('/api', require('./routes/schedules'));
+app.use('/api', require('./routes/deliveryreport'));
 
 // const deliveryRouter = require("./routes/deliveries");
 // //const deliveryRouter = require("./routes/deliveries.js");
@@ -137,11 +139,11 @@ app.use("/api", require("./routes/deliveryreport"));
 //Yasitha***************************************************************************
 
 //Routes
-app.use("/api", require("./routes/auth"));
-app.use("/api", require("./routes/category"));
-app.use("/api", require("./routes/stock"));
-app.use("/api", require("./routes/profit"));
-app.use("/api", require("./routes/stockreport"));
+app.use('/api', require('./routes/auth'));
+app.use('/api', require('./routes/category'));
+app.use('/api', require('./routes/stock'));
+app.use('/api', require('./routes/profit'));
+app.use('/api', require('./routes/stockreport'));
 
 //Yasitha***************************************************************************
 
