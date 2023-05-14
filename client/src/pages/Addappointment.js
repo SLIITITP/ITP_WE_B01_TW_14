de05273721396aa9dd407375184685f0d831ddf5
it21312908 <it21312908@my.sliit.lk>
2023-05-14 11:37:25 +0530

    AddAppointment form modified

--- client/src/pages/Addappointment.js
+++ client/src/pages/Addappointment.js
@@ -46,112 +46,107 @@ const handleClear = () => {
 
   return (
     <>
-      <h2>Create Appointment</h2>
-      <Card clasName="shadow card">
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="name" className="form-label mt-4">
-              Enter Supplier Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Brian Perera"
-              required
-              maxLength="25" // add maxLength attribute to limit input to 10 characters
-              fdprocessedid="8n2of"
-            />
-          </div>
+      <div className="d-flex justify-content-center">
+  <form onSubmit={handleSubmit} className="col-lg-6 col-md-8 col-12">
+    <div className="form-group">
+      <label htmlFor="name" className="form-label mt-4">
+        Enter Supplier Name
+      </label>
+      <input
+        type="text"
+        className="form-control"
+        id="name"
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Brian Perera"
+        required
+        maxLength="25"
+        fdprocessedid="8n2of"
+      />
+    </div>
 
-          <div className="form-group">
-            <div class="mb-3 col-lg-6 col-md-6 col-12">
-              <label htmlFor="date" className="form-label mt-4">
-                Enter Date
-              </label>
-              <input
-                type="date"
-                className="form-control"
-                id="date"
-                name="date"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                placeholder="Negombo"
-                required
-                maxLength="50"
-                fdprocessedid="8n2of"
-              />
-            </div>
-          </div>
+    <div className="form-group">
+      <label htmlFor="date" className="form-label mt-4">
+        Enter Date
+      </label>
+      <input
+        type="date"
+        className="form-control"
+        id="date"
+        name="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        placeholder="Negombo"
+        required
+        maxLength="50"
+        fdprocessedid="8n2of"
+      />
+    </div>
 
-          <div className="form-group">
-            <label htmlFor="start" className="form-label mt-4">
-              Enter Start-time
-            </label>
-            <input
-              type="time"
-              className="form-control"
-              id="start"
-              name="start"
-              value={start}
-              onChange={(e) => setStart(e.target.value)}
-              required
-              maxLength="10"
-              minLength="10"
-              fdprocessedid="8n2of"
-            />
-          </div>
+    <div className="form-group">
+      <label htmlFor="start" className="form-label mt-4">
+        Enter Start-time
+      </label>
+      <input
+        type="time"
+        className="form-control"
+        id="start"
+        name="start"
+        value={start}
+        onChange={(e) => setStart(e.target.value)}
+        required
+        maxLength="10"
+        minLength="10"
+        fdprocessedid="8n2of"
+      />
+    </div>
 
-          <div className="form-group">
-            <label htmlFor="end" className="form-label mt-4">
-              Enter End-time
-            </label>
-            <input
-              type="time"
-              className="form-control"
-              id="end"
-              name="end"
-              value={end}
-              onChange={(e) => setEnd(e.target.value)}
-              required
-              fdprocessedid="8n2of"
-            />
-          </div>
+    <div className="form-group">
+      <label htmlFor="end" className="form-label mt-4">
+        Enter End-time
+      </label>
+      <input
+        type="time"
+        className="form-control"
+        id="end"
+        name="end"
+        value={end}
+        onChange={(e) => setEnd(e.target.value)}
+        required
+        fdprocessedid="8n2of"
+      />
+    </div>
 
-          <div className="form-group">
-            <label htmlFor="email" className="form-label mt-4">
-              Enter Supplier Email
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="email"
-              //   name="company"
-              //   value={company}
-              //   onChange={(e) => setCompany(e.target.value)}
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="someone@gmail.com"
-              required
-              fdprocessedid="8n2of"
-            />
-          </div>
+    <div className="form-group">
+      <label htmlFor="email" className="form-label mt-4">
+        Enter Supplier Email
+      </label>
+      <input
+        type="text"
+        className="form-control"
+        id="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="someone@gmail.com"
+        required
+        fdprocessedid="8n2of"
+      />
+    </div>
 
-          <input type="submit" value="Submit" className="btn btn-info my-2" />
+    <input type="submit" value="Submit" className="btn btn-info my-2" />
 
-          <button
-            type="button"
-            onClick={handleClear}
-            className="btn btn-danger my-2 ml-2"
-          >
-            Clear
-          </button>
-        </form>
-      </Card>
+    <button
+      type="button"
+      onClick={handleClear}
+      className="btn btn-danger my-3 ml-2"
+    >
+      Clear
+    </button>
+  </form>
+</div>
+
     </>
   );
 };
