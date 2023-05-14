fafa34c27c76467fd7067769e2052eb7aac900c7
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2023-05-14 14:06:45 +0530

    new customer management styles

--- client/src/screens/ShippingDetailsScreen.js
+++ client/src/screens/ShippingDetailsScreen.js
@@ -63,107 +63,133 @@ export default function ShippingDetailsScreen() {
   };
 
   return (
-    <div>
-      <Helmet>
-        <title>Shipping Details</title>
-      </Helmet>
-      <div
-        className="container small-container"
-        style={{ fontWeight: '400', color: 'black' }}
-      >
-        <h1 className="my-3">Shipping Details</h1>
-        <Form onSubmit={submitHandler}>
-          <Form.Group className="mb-3" controlId="companyName">
-            <Form.Label>
-              <strong>Company Name</strong>
-            </Form.Label>
-            <Form.Control
-              type="text"
-              value={companyName}
-              onChange={(e) => setCompanyName(e.target.value)}
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="customerName">
-            <Form.Label>
-              <strong>Customer Name</strong>
-            </Form.Label>
-            <Form.Control
-              type="text"
-              value={customerName}
-              onChange={(e) => setCustomerName(e.target.value)}
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="shippingAddress">
-            <Form.Label>
-              <strong>Shipping Address</strong>
-            </Form.Label>
-            <Form.Control
-              type="text"
-              value={shippingAddress}
-              onChange={(e) => setShippingAddress(e.target.value)}
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="city">
-            <Form.Label>
-              <strong>City</strong>
-            </Form.Label>
-            <Form.Control
-              type="text"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="contactNo">
-            <Form.Label>
-              <strong>Contact Number</strong>
-            </Form.Label>
-            <Form.Control
-              type="tel"
-              maxLength={10}
-              minLength={10}
-              pattern="[0-9]{10}"
-              value={contactNo}
-              onChange={(e) => setContactNo(e.target.value)}
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="creditDays">
-            <Form.Label>
-              <strong>Credit Days </strong>
-            </Form.Label>
-            <br />
-            <select
-              value={creditDays}
-              onChange={(e) => setCreditDays(e.target.value)}
-              required
+    <center>
+      <div>
+        <Helmet>
+          <title>Shipping Details</title>
+        </Helmet>
+        <div
+          className="small-container"
+          style={{ fontWeight: '400', color: 'black' }}
+        >
+          <h1 className="my-3 text-center">Shipping Details</h1>
+          <Form onSubmit={submitHandler}>
+            <Form.Group
+              className="mb-3"
+              controlId="companyName"
+              style={{ textAlign: 'left' }}
             >
-              <option value="15">15</option>
-              <option value="30">30</option>
-              <option value="45">45</option>
-              <option value="60">60</option>
-              <option value="90">90</option>
-            </select>
-          </Form.Group>
-          <div className="mb-3 text-center">
-            <Button
-              variant="primary"
-              type="submit"
-              style={{
-                backgroundColor: '#f0c040',
-                color: 'black',
-                borderRadius: '10px',
-                border: '1px black solid',
-              }}
+              <Form.Label>
+                <strong>Company Name</strong>
+              </Form.Label>
+              <Form.Control
+                type="text"
+                value={companyName}
+                onChange={(e) => setCompanyName(e.target.value)}
+                required
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="customerName"
+              style={{ textAlign: 'left' }}
             >
-              Continue
-            </Button>
-          </div>
-        </Form>
+              <Form.Label>
+                <strong>Customer Name</strong>
+              </Form.Label>
+              <Form.Control
+                type="text"
+                value={customerName}
+                onChange={(e) => setCustomerName(e.target.value)}
+                required
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="shippingAddress"
+              style={{ textAlign: 'left' }}
+            >
+              <Form.Label>
+                <strong>Shipping Address</strong>
+              </Form.Label>
+              <Form.Control
+                type="text"
+                value={shippingAddress}
+                onChange={(e) => setShippingAddress(e.target.value)}
+                required
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="city"
+              style={{ textAlign: 'left' }}
+            >
+              <Form.Label>
+                <strong>City</strong>
+              </Form.Label>
+              <Form.Control
+                type="text"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                required
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="contactNo"
+              style={{ textAlign: 'left' }}
+            >
+              <Form.Label>
+                <strong>Contact Number</strong>
+              </Form.Label>
+              <Form.Control
+                type="tel"
+                maxLength={10}
+                minLength={10}
+                pattern="[0-9]{10}"
+                value={contactNo}
+                onChange={(e) => setContactNo(e.target.value)}
+                required
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="creditDays"
+              style={{ textAlign: 'left' }}
+            >
+              <Form.Label>
+                <strong>Credit Days </strong>
+              </Form.Label>
+              <br />
+              <select
+                value={creditDays}
+                onChange={(e) => setCreditDays(e.target.value)}
+                required
+              >
+                <option value="15">15</option>
+                <option value="30">30</option>
+                <option value="45">45</option>
+                <option value="60">60</option>
+                <option value="90">90</option>
+              </select>
+            </Form.Group>
+            <div className="mb-3 text-center">
+              <Button
+                variant="primary"
+                type="submit"
+                style={{
+                  backgroundColor: '#f0c040',
+                  color: 'black',
+                  borderRadius: '10px',
+                  border: '1px black solid',
+                }}
+              >
+                <b>Continue</b>
+              </Button>
+            </div>
+          </Form>
+        </div>
       </div>
-    </div>
+    </center>
   );
 }
