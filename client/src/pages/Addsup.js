2d2da59aadbb6199d20a48c03b83eda40955a2fe
it21312908 <it21312908@my.sliit.lk>
2023-05-14 11:26:10 +0530

    Addsup client modified

--- client/src/pages/Addsup.js
+++ client/src/pages/Addsup.js
@@ -67,110 +67,112 @@ const Addsup = () => {
     <Helmet>
         <title>Supplier</title>
       </Helmet>
-      <h2 className="text-center bg-darkgreen text-white p-2">Create Supplier</h2>
-      
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="supCompanyInput" className="form-label mt-4">
-              Enter Supplier Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="supCompanyInput"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Brian Perera"
-              required
-              maxLength="25" // add maxLength attribute to limit input to 10 characters
-              fdprocessedid="8n2of"
-            />
-          </div>
+      <div className="container d-flex justify-content-center align-items-center">
+  <div className="col-lg-6 col-md-8 col-12">
+    <h2 className="text-center bg-darkgreen text-white p-2">Create Supplier</h2>
 
-          <div className="form-group">
-            <div class="mb-3 col-lg-6 col-md-6 col-12">
-              <label htmlFor="supCompanyInput" className="form-label mt-4">
-                Enter Supplier Address
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="supCompanyInput"
-                name="address"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                placeholder="Negombo"
-                required
-                maxLength="50" 
-                fdprocessedid="8n2of"
-              />
-            </div>
-          </div>
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="supCompanyInput" className="form-label mt-4">
+          Enter Supplier Name
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="supCompanyInput"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Brian Perera"
+          required
+          maxLength="25"
+          fdprocessedid="8n2of"
+        />
+      </div>
 
-          <div className="form-group">
-            <label htmlFor="supCompanyInput" className="form-label mt-4">
-              Enter Supplier Mobile
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="supCompanyInput"
-              name="mobile"
-              value={mobile}
-              onChange={(e) => setMobile(e.target.value)}
-              placeholder="07xxxxxxxx"
-              required
-              maxLength="10" 
-              minLength="10"
-              fdprocessedid="8n2of"
-            />
-          </div>
+      <div className="form-group">
+        <label htmlFor="supCompanyInput" className="form-label mt-4">
+          Enter Supplier Address
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="supCompanyInput"
+          name="address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+          placeholder="Negombo"
+          required
+          maxLength="50"
+          fdprocessedid="8n2of"
+        />
+      </div>
 
-          <div className="form-group">
-            <label htmlFor="supCompanyInput" className="form-label mt-4">
-              Enter Supplier Email
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="supCompanyInput"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="someone@gmail.com"
-              required
-              fdprocessedid="8n2of"
-            />
-          </div>
+      <div className="form-group">
+        <label htmlFor="supCompanyInput" className="form-label mt-4">
+          Enter Supplier Mobile
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="supCompanyInput"
+          name="mobile"
+          value={mobile}
+          onChange={(e) => setMobile(e.target.value)}
+          placeholder="07xxxxxxxx"
+          required
+          maxLength="10"
+          minLength="10"
+          fdprocessedid="8n2of"
+        />
+      </div>
 
-          <div className="form-group">
-            <label htmlFor="supCompanyInput" className="form-label mt-4">
-              Enter Supplier Company
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="supCompanyInput"
-              name="company"
-              value={company}
-              onChange={(e) => setCompany(e.target.value)}
-              placeholder="CIC"
-              required
-              fdprocessedid="8n2of"
-            />
-          </div>
+      <div className="form-group">
+        <label htmlFor="supCompanyInput" className="form-label mt-4">
+          Enter Supplier Email
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="supCompanyInput"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="someone@gmail.com"
+          required
+          fdprocessedid="8n2of"
+        />
+      </div>
 
-          <input type="submit" value="Submit" className="btn btn-info my-2" />
+      <div className="form-group">
+        <label htmlFor="supCompanyInput" className="form-label mt-4">
+          Enter Supplier Company
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="supCompanyInput"
+          name="company"
+          value={company}
+          onChange={(e) => setCompany(e.target.value)}
+          placeholder="CIC"
+          required
+          fdprocessedid="8n2of"
+        />
+      </div>
+        <input type="submit" value="Submit" className="btn btn-info my-4 mr-2" />
 
-          <button
-            type="button"
-            onClick={handleClear}
-            className="btn btn-danger my-2 ml-2"
-          >
-            Clear
-          </button>
-        </form>
+        <button
+          type="button"
+          onClick={handleClear}
+          className="btn btn-danger my-2 ml-2"
+        >
+          Clear
+        </button>
+    </form>
+  </div>
+</div>
+
     </>
   );
 };
